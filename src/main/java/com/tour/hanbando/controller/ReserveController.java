d8e29faac73bd80293b2328acbf490a754efe04f
Bell-212 <helloj0212@gmail.com>
2023-11-27 14:24:12 +0900

    Modify reserve/list.html

--- src/main/java/com/tour/hanbando/controller/ReserveController.java
+++ src/main/java/com/tour/hanbando/controller/ReserveController.java
@@ -1,6 +1,9 @@
 package com.tour.hanbando.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,9 +19,9 @@ public class ReserveController {
   private final ReserveService reserveService;
   
   @GetMapping("/list.do")
-  public String list() {
+  public String list(HttpServletRequest request, Model model) {
+    
     return "reserve/list";
   }
-  
   
 }
