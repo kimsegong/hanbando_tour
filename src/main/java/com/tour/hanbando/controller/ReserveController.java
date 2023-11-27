c0d023ed4ca9a927324c0d624f9c86318782a660
Bell-212 <helloj0212@gmail.com>
2023-11-27 13:51:44 +0900

    Add reserve html

--- src/main/java/com/tour/hanbando/controller/ReserveController.java
+++ src/main/java/com/tour/hanbando/controller/ReserveController.java
@@ -1,8 +1,24 @@
 package com.tour.hanbando.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tour.hanbando.service.ReserveService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/reserve")
+@RequiredArgsConstructor
 @Controller
 public class ReserveController {
 
+  private final ReserveService reserveService;
+  
+  @GetMapping("/list.do")
+  public String list() {
+    return "reserve/list";
+  }
+  
+  
 }
