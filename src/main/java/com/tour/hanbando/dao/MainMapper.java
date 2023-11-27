f3e6b47247c8947b612bd9532b09795b6259f9d1
skal48 <skal48@naver.com>
2023-11-28 00:24:56 +0900

    Add headerSearch.html

--- src/main/java/com/tour/hanbando/dao/MainMapper.java
+++ src/main/java/com/tour/hanbando/dao/MainMapper.java
@@ -1,8 +1,14 @@
 package com.tour.hanbando.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.tour.hanbando.dto.PackageDto;
 
 @Mapper
 public interface MainMapper {
-
+  public List<PackageDto> searchPackageList(Map<String, Object> map);
+  public int countSearchPackage(Map<String, Object> map);
 }
