0d7451a7c27ebe40672526abb677fb1a60252b4c
Bell-212 <helloj0212@gmail.com>
2023-11-28 11:23:46 +0900

    Add reserveList

--- src/main/java/com/tour/hanbando/service/ReserveService.java
+++ src/main/java/com/tour/hanbando/service/ReserveService.java
@@ -1,5 +1,11 @@
 package com.tour.hanbando.service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
 public interface ReserveService {
 
+  public void loadReserveList(HttpServletRequest request, Model model);
+  
 }
