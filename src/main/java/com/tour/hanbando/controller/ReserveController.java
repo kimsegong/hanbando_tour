fa637978a03bedc584a3990d21476a2d5e87a5c2
Bell-212 <helloj0212@gmail.com>
2023-11-28 16:17:04 +0900

    Add feature ReserveList, ReserveDetail

--- src/main/java/com/tour/hanbando/controller/ReserveController.java
+++ src/main/java/com/tour/hanbando/controller/ReserveController.java
@@ -1,12 +1,17 @@
 package com.tour.hanbando.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tour.hanbando.dto.ReserveDto;
 import com.tour.hanbando.service.ReserveService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +25,11 @@ public class ReserveController {
   
   @GetMapping("/reserveList.do")
   public String list(HttpServletRequest request, Model model) {
-    reserveService.loadReserveList(request, model);
+    if(request.getParameter("userNo") != null) { // userNo가 파라미터로 넘어오는 경우 
+      reserveService.loadReserveListByUser(request, model);
+    } else { // 파라미터가 없는 경우
+      reserveService.loadReserveList(request, model);
+    }
     return "reserve/list";
   }
   
@@ -29,8 +38,10 @@ public class ReserveController {
     return "reserve/write";
   }
   
-  @RequestMapping("/detail.do")
-  public String detail(HttpServletRequest request, Model model) {
+  @GetMapping("/detail.do")
+  public String detail(@RequestParam(value="reserveNo", required = false, defaultValue = "0") int reserveNo, Model model) {
+    ReserveDto reserve = reserveService.loadReserve(reserveNo);
+    model.addAttribute("reserve", reserve);
     return "reserve/detail";
   }
   
@@ -38,5 +49,15 @@ public class ReserveController {
   public String edit() {
     return "reserve/edit";
   }
+ 
+  
+  @ResponseBody
+  @GetMapping(value="/getTouristInfo.do", produces="application/json")
+  public Map<String, Object> getTourists(HttpServletRequest request) {
+    return reserveService.loadTourists(request);
+  }
+  
+  
+  
   
 }
