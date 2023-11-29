482517e801d16b267a5f64555215486cb3529943
bvbvbv42 <bvmn2@naver.com>
2023-11-29 17:54:07 +0900

    Add my html

--- src/main/java/com/tour/hanbando/dao/NoticeMapper.java
+++ src/main/java/com/tour/hanbando/dao/NoticeMapper.java
@@ -1,5 +1,18 @@
 package com.tour.hanbando.dao;
 
-public class NoticeMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tour.hanbando.dto.NoticeDto;
+
+@Mapper
+public interface NoticeMapper {
+  public List<NoticeDto> getNoticeList(Map<String, Object> map);
+  public int getNoticeCount();
+  public List<NoticeDto> getSearchList(Map<String, Object> map);
+  public int getSearchCount(Map<String, Object> map);
+  public int insertNotice(NoticeDto notice);
+  public NoticeDto getNotice(int noticeNo);
 }
