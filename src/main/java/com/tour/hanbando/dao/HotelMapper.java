b9f7c9cdd995bfae09c1493408a0690ea2b62124
skal48 <skal48@naver.com>
2023-11-30 17:36:30 +0900

    Not yet complete

--- src/main/java/com/tour/hanbando/dao/HotelMapper.java
+++ src/main/java/com/tour/hanbando/dao/HotelMapper.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.tour.hanbando.dto.HotelDto;
+import com.tour.hanbando.dto.RoompriceDto;
 
 @Mapper
 public interface HotelMapper {
   public List<HotelDto> selectHotelList(Map<String, Object> map);
   public int countHotel();
+  public List<RoompriceDto> getListPrice(List<HotelDto> hotelDto);
 
 }
