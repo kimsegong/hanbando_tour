d0f15b370ed94c88b2d2ab04b516595d5d133311
skal48 <skal48@naver.com>
2023-11-30 14:20:22 +0900

    sdf

--- src/main/java/com/tour/hanbando/dao/HotelMapper.java
+++ src/main/java/com/tour/hanbando/dao/HotelMapper.java
@@ -1,8 +1,15 @@
 package com.tour.hanbando.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.tour.hanbando.dto.HotelDto;
 
 @Mapper
 public interface HotelMapper {
+  public List<HotelDto> selectHotelList(Map<String, Object> map);
+  public int countHotel();
 
 }
--- src/main/java/com/tour/hanbando/service/HotelServiceImpl.java
+++ src/main/java/com/tour/hanbando/service/HotelServiceImpl.java
@@ -1,9 +1,17 @@
 package com.tour.hanbando.service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tour.hanbando.dao.HotelMapper;
+import com.tour.hanbando.dto.PackageDto;
+import com.tour.hanbando.util.MyPageUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +20,31 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class HotelServiceImpl implements HotelService {
   private final HotelMapper hotelMapper;
+  private final MyPageUtils myPageUtils;
+  
+  
+  @Override
+  public Map<String, Object> getHotelList(HttpServletRequest request) {
+    
+    Optional<String> opt = Optional.ofNullable(request.getParameter("page"));
+    int page = Integer.parseInt(opt.orElse("1"));
+    int total = hotelMapper.countHotel();
+    int display = 9;
+    
+    myPageUtils.setPaging(page, total, display);
+    
+    Map<String, Object> map = Map.of("begin", myPageUtils.getBegin()
+                                   , "end", myPageUtils.getEnd());
+              
+    
+    Map<String, Object> hotel = Map.of("hotelList", hotelMapper.selectHotelList(map)
+                                      ,"count", hotelMapper.countHotel()
+                                      ,"totalPage", myPageUtils.getTotalPage());
+     
+    
+    
+    
+    return hotel;
+  }
   
 }
