fa47b7de480767f7b111dc12d011690d013d69a6
bvbvbv42 <bvmn2@naver.com>
2023-12-01 09:05:17 +0900

    Add my html

--- src/main/java/com/tour/hanbando/controller/InquiryController.java
+++ src/main/java/com/tour/hanbando/controller/InquiryController.java
@@ -1,9 +1,13 @@
 package com.tour.hanbando.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tour.hanbando.service.InquiryService;
 
@@ -17,13 +21,22 @@ public class InquiryController {
   private final InquiryService inquiryService;
   
   @GetMapping("/inquirylist.do")
-  public String inquirylist() {
+  public String inquirylist(HttpServletRequest request, Model model) {
+    inquiryService.loadInquiryList(request, model);
     return "notice/inquirylist";
   }
   
   @PostMapping("/addInquiry.do")
-  public String addInquiry() {
+  public String addInquiry(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    int addResult = inquiryService.addInquiry(request);
+    redirectAttributes.addFlashAttribute("addResult", addResult);
     return "redirect:/notice/inquirylist.do";
+  }
+  
+  @GetMapping("/inquirywrite.form")
+  public String inquirywrite() {
+    return "notice/inquirywrite";
+  
   }
 
 }
--- src/main/java/com/tour/hanbando/service/InquiryService.java
+++ src/main/java/com/tour/hanbando/service/InquiryService.java
@@ -5,11 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
 
 import com.tour.hanbando.dto.InquiryDto;
-import com.tour.hanbando.dto.NoticeDto;
 
 public interface InquiryService {
   public void loadInquiryList(HttpServletRequest request, Model model);
-  public int addInquiry(HttpServletRequest request, Model model);
+  public int addInquiry(HttpServletRequest request);
   public InquiryDto loadInquiry(int inquiryNo);
 }
 
