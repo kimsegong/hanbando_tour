8167bf79df7ad3f5fb3258ea836d2f0b5237f536
bvbvbv42 <bvmn2@naver.com>
2023-12-06 14:49:00 +0900

    Add mt to html

--- src/main/java/com/tour/hanbando/controller/InquiryController.java
+++ src/main/java/com/tour/hanbando/controller/InquiryController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.tour.hanbando.dto.InquiryDto;
 import com.tour.hanbando.service.InquiryService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,7 @@ public class InquiryController {
     return "notice/inquirylist";
   }
   
+  /* 1:1문의 작성하기 */
   @PostMapping("/addInquiry.do")
   public String addInquiry(HttpServletRequest request, RedirectAttributes redirectAttributes) {
     int addResult = inquiryService.addInquiry(request);
@@ -33,12 +36,34 @@ public class InquiryController {
     return "redirect:/notice/inquirylist.do";
   }
   
-
-  
   @GetMapping("/inquirywrite.form")
   public String inquirywrite() {
     return "notice/inquirywrite";
   
+  }
+  
+  @PostMapping("/inquiryremove.do")
+  public String inquiryremove(@RequestParam(value="inquiryNo", required=false, defaultValue="0") int inquiryNo
+                     , RedirectAttributes redirectAttributes) {
+    int removeResult = inquiryService.removeInquiry(inquiryNo);
+    redirectAttributes.addFlashAttribute("removeResult", removeResult);
+    return "redirect:/notice/inquirylist.do";
+  
+ }
+  
+  @GetMapping("/inquirydetail.do")
+  public String inquirydetail(@RequestParam(value="inquiryNo", required=false, defaultValue="0") int inquiryNo 
+      , Model model){
+    InquiryDto inquiry = inquiryService.loadInquiry(inquiryNo);
+    model.addAttribute("inquiry", inquiry);
+    return "notice/inquirydetail";
+  }
+  
+  @PostMapping("/modifyInquiry.do")
+  public String modifyInquiry(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    int modifyResult = inquiryService.modifyInquiry(request);
+    redirectAttributes.addFlashAttribute("modifyResult", modifyResult);
+    return "redirect:/notice/inquirydetail.do?inquiryNo=" + request.getParameter("inquiryNo");
   }
 
 }
--- src/main/java/com/tour/hanbando/dao/InquiryMapper.java
+++ src/main/java/com/tour/hanbando/dao/InquiryMapper.java
@@ -6,10 +6,13 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.tour.hanbando.dto.InquiryDto;
+import com.tour.hanbando.dto.NoticeDto;
 
 @Mapper
 public interface InquiryMapper {
-  public List<InquiryDto> getInquiryList(Map<String, Object> map);
+  public List<InquiryDto> getInquiryList();
   public int insertInquiry(InquiryDto inquiry);
   public InquiryDto getInquiry(int inquiryNo);
+  public int deleteInquiry(int inquiryNo);
+  public int updateInquiry(InquiryDto inquiry);
 }
